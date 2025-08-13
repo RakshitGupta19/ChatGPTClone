b91aede7435fb00a33b1fb828365c47feb438eff
Rakshit <rakshit.gupta229@gmail.com>
2025-08-13 10:12:33 +0530

    Version 1 Ready for Deployment

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+import ChatWindow from './ChatWindow';
+import { MyContext } from './MyContext';
+import {v1 as uuidv1} from "uuid";
+import './App.css';
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const [prevChats, setPrevChats] = useState([]);
+  const [newChat, setNewChat] = useState(true);
+  const [allThreads, setAllThreads] = useState([]);
+
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId,
+    prevChats, setPrevChats,
+    newChat, setNewChat,
+    allThreads, setAllThreads
+  };
+
+  return (
+    <div className='app'>
+      <MyContext.Provider value={providerValues}>
+        <Sidebar />
+        <ChatWindow />
+      </MyContext.Provider>
+    </div>
+  );
+}
+
+export default App;
